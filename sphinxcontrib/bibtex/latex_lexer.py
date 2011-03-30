4065951656140f323d85350020ac772ff5d2a22c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2011-03-30 14:12:12 +0100

    Error handling, unicode decoding.

--- sphinxcontrib/bibtex/latex_lexer.py
+++ sphinxcontrib/bibtex/latex_lexer.py
@@ -1,8 +1,9 @@
 """
-    Simple Tex Lexer
-    ~~~~~~~~~~~~~~~~~~
+    Simple incremental latex lexer
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
+import codecs
 import re
 
 class Token:
@@ -15,7 +16,16 @@ class Token:
     def __nonzero__(self):
         return bool(self.text)
 
-class TexLexer:
+    def __len__(self):
+        return len(self.text)
+
+    def decode(self, encoding):
+        if self.name == 'control_word':
+            return self.text.decode(encoding) + u' '
+        else:
+            return self.text.decode(encoding)
+
+class LatexIncrementalDecoder(codecs.IncrementalDecoder):
     """A very simple incremental lexer for tex/latex code. Roughly
     follows the state machine descriped in Tex By Topic, Chapter 2.
 
@@ -23,17 +33,27 @@ class TexLexer:
 
     * no newline characters: paragraphs are separated by '\\par'
     * spaces following control tokens are compressed
+
+    The easiest way to customize decoding is to subclass and
+    overriding :meth:`get_unicode_tokens`.
     """
+
+    inputenc = "ascii"
+    """Default input encoding. **Must** extend ascii."""
+
+    # implementation note: every token **must** be decodable by inputenc
     tokens = [
         # comment: for ease, and for speed, we handle it as a token
         ('comment', br'%.*?\n'),
         # control tokens
-        # in latex, some control tokens skip following whitespace ('control')
-        # others do not ('controlx')
+        # in latex, some control tokens skip following whitespace
+        # ('control-word' and 'control-symbol')
+        # others do not ('control-symbol-x')
         # XXX TBT says no control symbols skip whitespace (except '\ ')
         # XXX but tests reveal otherwise?
-        ('control', br'[\\]([a-zA-Z]+|[~' + ur"'" + ur'"` =^!])'),
-        ('controlx', br'[\\][^a-zA-Z]'),
+        ('control_word', br'[\\][a-zA-Z]+'),
+        ('control_symbol', br'[\\][~' + ur"'" + ur'"` =^!]'),
+        ('control_symbol_x', br'[\\][^a-zA-Z]'), # TODO should only match ascii
         # parameter tokens
         ('parameter', br'\#[0-9]'),
         # any remaining characters; for ease we also handle space and
@@ -41,6 +61,7 @@ class TexLexer:
         ('space', br' '),
         ('newline', br'\n'),
         ('mathshift', br'[$]'),
+        # note: chars joined together for speed, and to ensure decodability
         ('chars', br'([^ %#$\n\\])+'),
         # trailing garbage which we cannot decode otherwise
         # (such as a lone '\' at the end of a buffer)
@@ -48,8 +69,20 @@ class TexLexer:
         ('unknown', br'.'),
         ]
 
-    def __init__(self):
-        """Initialize regular expression string."""
+    def __init__(self, errors='strict'):
+        """Initialize the codec."""
+        codecs.IncrementalDecoder.__init__(self, errors)
+        # regular expression used for matching
+        self.regexp = re.compile(
+            b"|".join(
+                b"(?P<%s>%s)" % (name, regexp)
+                for name, regexp in self.tokens),
+            re.DOTALL)
+        # reset state
+        self.reset()
+
+    def reset(self):
+        """Reset state."""
         # three possible states:
         # newline (N), skipping spaces (S), and middle of line (M)
         self.state = 'N'
@@ -57,12 +90,8 @@ class TexLexer:
         self.inline_math = False
         # buffer for storing last (possibly incomplete) token
         self.buffer_ = Token()
-        # regular expression used for matching
-        self.regexp = re.compile(
-            b"|".join(
-                b"(?P<%s>%s)" % (name, regexp)
-                for name, regexp in self.tokens),
-            re.DOTALL)
+        # keeps track of how many bytes have been consumed in total
+        self.consumed = 0
 
     def get_raw_tokens(self, bytes_, final=False):
         """Yield tokens without any further processing. Tokens are one of:
@@ -93,10 +122,11 @@ class TexLexer:
                 yield token
 
     def flush_raw_tokens(self):
-        """Flush the raw token buffer."""
+        """Flush the raw token buffer, and update number of consumed
+        bytes.
+        """
         if self.buffer_:
-            if self.buffer_.name == 'unknown':
-                raise AssertionError("unknown token: lexer bug?")
+            self.consumed += len(self.buffer_)
             yield self.buffer_
             self.buffer_ = Token()
 
@@ -106,11 +136,14 @@ class TexLexer:
         Replaces newlines by spaces and \\par commands depending on
         the context.
         """
+        # mark the start of bytes_ in the sequence of all decoded bytes
+        # so far (we need this when reporting errors)
+        start = self.consumed + len(self.buffer_)
         for token in self.get_raw_tokens(bytes_, final=final):
             if token.name == 'newline':
                 if self.state == 'N':
                     # if state was 'N', generate new paragraph
-                    yield Token('control', '\\par')
+                    yield Token('control_word', '\\par')
                 elif self.state == 'S':
                     # switch to 'N' state, do not generate a space
                     self.state = 'N'
@@ -145,11 +178,15 @@ class TexLexer:
             elif token.name == 'parameter':
                 self.state = 'M'
                 yield token
-            elif token.name == 'control':
+            elif token.name == 'control_word':
                 # go to space skip mode
                 self.state = 'S'
                 yield token
-            elif token.name == 'controlx':
+            elif token.name == 'control_symbol':
+                # go to space skip mode
+                self.state = 'S'
+                yield token
+            elif token.name == 'control_symbol_x':
                 # don't skip following space, so go to M mode
                 self.state = 'M'
                 yield token
@@ -160,6 +197,40 @@ class TexLexer:
             elif token.name == 'chars':
                 self.state = 'M'
                 yield token
-            else:
-                raise AssertionError(
-                    "unknown token name '%s'" % token.name)
+            elif token.name == 'unknown':
+                if self.errors == 'strict':
+                    # current position within bytes_
+                    # this is the position right after the unknown token
+                    pos = self.consumed - start
+                    raise UnicodeDecodeError(
+                        "latex", # codec
+                        bytes_, # problematic input
+                        pos - len(token), # start of problematic token
+                        pos, # end of it
+                        "unknown token %s" % repr(self.buffer_.text))
+                elif self.errors == 'ignore':
+                    # do nothing
+                    pass
+                elif self.errors == 'replace':
+                    yield Token('chars', b'?' * len(token))
+                else:
+                    raise NotImplementedError(
+                        "error mode %s not supported" % repr(self.errors))
+
+    def get_unicode_tokens(self, bytes_, final=False):
+        """:meth:`decode` calls this function to produce the final
+        sequence of unicode strings. This implementation simply
+        decodes every sequence in *inputenc* encoding. Override to
+        process the tokens in some other way (for example, for token
+        translation).
+        """
+        return (token.decode(self.inputenc)
+                for token in self.get_tokens(bytes_, final=final))
+
+    def decode(self, bytes_, final=False):
+        try:
+            return u''.join(self.get_unicode_tokens(bytes_, final=final))
+        except UnicodeDecodeError, e:
+            # API requires that the encode method raises a ValueError
+            # in this case
+            raise ValueError(e)
--- test/test_latex_lexer.py
+++ test/test_latex_lexer.py
@@ -1,22 +1,34 @@
 """Tests for the tex lexer."""
 
+import nose.tools
 from unittest import TestCase
 
-from sphinxcontrib.bibtex.latex_lexer import TexLexer
+from sphinxcontrib.bibtex.latex_lexer import LatexIncrementalDecoder
 
-class TexLexerTest(TestCase):
+class BaseTexLexerTest(TestCase):
     """Tex lexer fixture."""
-    def setUp(self):
-        self.lexer = TexLexer()
 
-    def lex_it(self, latex_code, latex_tokens):
-        tokens = self.lexer.get_tokens(latex_code, final=True)
+    errors = 'strict'
+
+    def setUp(self):
+        self.lexer = LatexIncrementalDecoder(self.errors)
+
+    def lex_it(self, latex_code, latex_tokens, consumed=None, final=False):
+        tokens = self.lexer.get_tokens(latex_code, final=final)
         self.assertEqual(
             list(token.text for token in tokens),
             latex_tokens)
+        if consumed is None:
+            consumed = len(latex_code)
+        self.assertEqual(self.lexer.consumed, consumed)
+
+    def tearDown(self):
+        del self.lexer
+
+class TexLexerTest(BaseTexLexerTest):
 
     def test_null(self):
-        self.lex_it(b'', [])
+        self.lex_it(b'', [], final=True)
 
     def test_hello(self):
         self.lex_it(
@@ -24,41 +36,116 @@ class TexLexerTest(TestCase):
             b'    \nHey.\n\n\# x \#x',
             br'hello!| |[|#1|]| |This| |\is|\ |\^|a| '
             br'|test.| |\par|Hey.| '
-            br'|\par|\#| |x| |\#|x'.split(b'|')
+            br'|\par|\#| |x| |\#|x'.split(b'|'),
+            final=True
             )
 
     def test_comment(self):
         self.lex_it(
             b'test% some comment\ntest',
-            b'test|test'.split(b'|')
+            b'test|test'.split(b'|'),
+            final=True
             )
 
     def test_comment_newline(self):
         self.lex_it(
             b'test% some comment\n\ntest',
-            b'test|\\par|test'.split(b'|')
+            b'test|\\par|test'.split(b'|'),
+            final=True
             )
 
     def test_control(self):
         self.lex_it(
             b'\\hello\\world',
-            b'\\hello|\\world'.split(b'|')
+            b'\\hello|\\world'.split(b'|'),
+            final=True
             )
 
     def test_control_whitespace(self):
         self.lex_it(
             b'\\hello   \\world   ',
-            b'\\hello|\\world'.split(b'|')
+            b'\\hello|\\world'.split(b'|'),
+            final=True
             )
 
     def test_controlx(self):
         self.lex_it(
             b'\\#\\&',
-            b'\\#|\\&'.split(b'|')
+            b'\\#|\\&'.split(b'|'),
+            final=True
             )
 
     def test_controlx_whitespace(self):
         self.lex_it(
             b'\\#    \\&   ',
-            b'\\#| |\\&| '.split(b'|')
+            b'\\#| |\\&| '.split(b'|'),
+            final=True
             )
+
+    def test_buffer(self):
+        self.lex_it(
+            b'hi\\t',
+            [b'hi'],
+            consumed=2,
+            )
+        self.lex_it(
+            b'here',
+            [b'\\there'],
+            consumed=8,
+            final=True,
+            )
+
+    def test_buffer_decode(self):
+        self.assertEqual(
+            self.lexer.decode(b'hello!  [#1] This \\i'),
+            u'hello! [#1] This ',
+            )
+        self.assertEqual(
+            self.lexer.decode(b's\\   \\^ a \ntest.\n'),
+            u'\\is \\ \\^a test.',
+            )
+        self.assertEqual(
+            self.lexer.decode(b'    \nHey.\n\n\# x \#x', final=True),
+            u' \\par Hey. \\par \\# x \\#x',
+            )
+
+    # counterintuitive?
+    @nose.tools.raises(UnicodeDecodeError)
+    def test_final_backslash(self):
+        self.lex_it(
+            b'notsogood\\',
+            [b'notsogood'],
+            final=True
+            )
+
+    # counterintuitive?
+    @nose.tools.raises(UnicodeDecodeError)
+    def test_final_comment(self):
+        self.lex_it(
+            b'hello%',
+            [b'hello'],
+            final=True
+            )
+
+class TexLexerReplaceTest(BaseTexLexerTest):
+
+    errors = 'replace'
+
+    def test_errors_replace(self):
+        self.lex_it(
+            b'hello%',
+            [b'hello', b'?'],
+            final=True
+            )
+        
+class TexLexerIgnoreTest(BaseTexLexerTest):
+
+    errors = 'ignore'
+
+    def test_errors_ignore(self):
+        self.lex_it(
+            b'hello%',
+            [b'hello'],
+            final=True
+            )
+        
