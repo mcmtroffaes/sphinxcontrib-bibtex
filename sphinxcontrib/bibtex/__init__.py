180bb90ee95d71020c16faccc4b5436cc95daa07
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2011-11-18 18:08:21 +0000

    Translate citation labels into consequetive numbers.

--- sphinxcontrib/bibtex/__init__.py
+++ sphinxcontrib/bibtex/__init__.py
@@ -8,18 +8,22 @@
     .. autofunction:: purge_bibtex_cache
 """
 
+import docutils.nodes
 from sphinxcontrib.bibtex.cache import Cache
 from sphinxcontrib.bibtex.roles import CiteRole
 from sphinxcontrib.bibtex.directives import BibliographyDirective
 
 def init_bibtex_cache(app):
     """Create ``app.env.bibtex_cache`` if it does not exist yet.
+    Reset citation label dictionary.
 
     :param app: The sphinx application.
     :type app: :class:`sphinx.application.Sphinx`
     """
     if not hasattr(app.env, "bibtex_cache"):
         app.env.bibtex_cache = Cache()
+    # XXX labels are currently not cached, always calculated on the fly
+    app.env.bibtex_citation_label = {}
 
 def purge_bibtex_cache(app, env, docname):
     """Remove all information related to *docname* from the cache.
@@ -30,6 +34,44 @@ def purge_bibtex_cache(app, env, docname):
     :type env: :class:`sphinx.environment.BuildEnvironment`
     """
     env.bibtex_cache.purge(docname)
+
+def process_citations(app, doctree, docname):
+    """Replace labels of citation nodes by numbers.
+
+    :param app: The sphinx application.
+    :type app: :class:`sphinx.application.Sphinx`
+    :param doctree: The document tree.
+    :type doctree: :class:`docutils.nodes.document`
+    :param docname: The document name.
+    :type docname: :class:`str`
+    """
+    for node in doctree.traverse(docutils.nodes.citation):
+        label = node[0].astext()
+        try:
+            num = app.env.bibtex_citation_label[label]
+        except KeyError:
+            num = str(len(app.env.bibtex_citation_label) + 1)
+            app.env.bibtex_citation_label[label] = num
+        node[0] = docutils.nodes.label('', num)
+
+def process_citation_references(app, doctree, docname):
+    """Replace text of citation reference nodes by numbers.
+
+    :param app: The sphinx application.
+    :type app: :class:`sphinx.application.Sphinx`
+    :param doctree: The document tree.
+    :type doctree: :class:`docutils.nodes.document`
+    :param docname: The document name.
+    :type docname: :class:`str`
+    """
+    # XXX sphinx has already turned citation_reference nodes
+    # XXX into reference nodes
+    for node in doctree.traverse(docutils.nodes.reference):
+        text = node[0].astext()
+        if text.startswith('[') and text.endswith(']'):
+            label = text[1:-1]
+            node[0] = docutils.nodes.Text(
+                '[' + app.env.bibtex_citation_label[label] + ']')
 
 def setup(app):
     """Set up the bibtex extension:
@@ -46,4 +88,6 @@ def setup(app):
     app.add_directive("bibliography", BibliographyDirective)
     app.add_role("cite", CiteRole())
     app.connect("builder-inited", init_bibtex_cache)
+    app.connect("doctree-resolved", process_citations)
+    app.connect("doctree-resolved", process_citation_references)
     app.connect("env-purge-doc", purge_bibtex_cache)
