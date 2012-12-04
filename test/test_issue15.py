9f123af3b338795700a5867354e2eec27240c8a3
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2012-12-04 10:18:19 +0000

    Add test to reproduce issue #15.

---
+++ test/test_issue15.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+    test_issue15
+    ~~~~~~~~~~~~
+
+    Test order of bibliography entries when using an unsorted style.
+"""
+
+import nose.tools
+from StringIO import StringIO
+import os.path
+import re
+
+from util import *
+
+srcdir = path(__file__).parent.joinpath('issue15').abspath()
+warnfile = StringIO()
+
+def teardown_module():
+    (srcdir / '_build').rmtree(True)
+
+@with_app(srcdir=srcdir, warningiserror=True)
+def test_duplicate_label(app):
+    app.builder.build_all()
+    with open(os.path.join(app.outdir, "index.html")) as stream:
+        assert re.search(
+            '<tr>.*Test 1.*</tr>.*<tr>.*Test 2.*</tr>',
+            stream.read(), re.DOTALL)
