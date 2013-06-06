6597c7f817aa43789502dd1fc4e1de0a910de3a3
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-06-06 15:25:05 +0100

    Test enumerated list, and its start option.

---
+++ test/list_enumerated/index.rst
@@ -0,0 +1,14 @@
+.. bibliography:: test.bib
+   :list: enumerated
+   :all:
+
+.. bibliography:: test2.bib
+   :list: enumerated
+   :start: continue
+   :all:
+
+.. bibliography:: test3.bib
+   :list: enumerated
+   :start: 23
+   :all:
+
---
+++ test/test_list_enumerated.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+    test_list_enumerated
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Test the ``:list: enumerated`` option.
+"""
+
+import nose.tools
+from StringIO import StringIO
+import os.path
+import re
+
+from util import *
+
+srcdir = path(__file__).parent.joinpath('list_enumerated').abspath()
+
+def teardown_module():
+    (srcdir / '_build').rmtree(True)
+
+@with_app(srcdir=srcdir, warningiserror=True)
+def test_encoding(app):
+    app.builder.build_all()
+    with open(os.path.join(app.outdir, "index.html")) as stream:
+        assert re.search(
+            '<ol .*id="bibtex-bibliography-index-0".* start="1".*>'
+            '.*<li>.*Akkerdju.*</li>'
+            '.*<li>.*Bro.*</li>'
+            '.*<li>.*Chap.*</li>'
+            '.*<li>.*Dude.*</li>'
+            '.*</ol>'
+            '.*<ol .*id="bibtex-bibliography-index-1".* start="5".*>'
+            '.*<li>.*Eminence.*</li>'
+            '.*<li>.*Frater.*</li>'
+            '.*<li>.*Giggles.*</li>'
+            '.*<li>.*Handy.*</li>'
+            '.*</ol>'
+            '.*<ol .*id="bibtex-bibliography-index-2".* start="23".*>'
+            '.*<li>.*Iedereen.*</li>'
+            '.*<li>.*Joke.*</li>'
+            '.*<li>.*Klopgeest.*</li>'
+            '.*<li>.*Laterfanter.*</li>'
+            '.*</ol>'
+            ,
+            stream.read(), re.MULTILINE | re.DOTALL)
