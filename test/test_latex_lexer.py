6a72be32a094778fa4e4dd6308e7dcf0a1b4c2e7
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-06-06 09:12:46 +0100

    Extra raw lexer tests, also test state.

--- test/test_latex_lexer.py
+++ test/test_latex_lexer.py
@@ -3,7 +3,7 @@
 import nose.tools
 from unittest import TestCase
 
-from sphinxcontrib.bibtex.latex_lexer import LatexIncrementalDecoder, Token
+from sphinxcontrib.bibtex.latex_lexer import LatexLexer, LatexIncrementalDecoder, Token
 
 def test_token_create():
     t = Token()
@@ -29,6 +29,130 @@ def test_token_assign_text():
 def test_token_assign_other():
     t = Token()
     t.blabla = 'test'
+
+class BaseLatexLexerTest(TestCase):
+
+    errors = 'strict'
+
+    def setUp(self):
+        self.lexer = LatexLexer(errors=self.errors)
+
+    def lex_it(self, latex_code, latex_tokens, final=False):
+        tokens = self.lexer.get_raw_tokens(latex_code, final=final)
+        self.assertEqual(
+            list(token.text for token in tokens),
+            latex_tokens)
+
+    def tearDown(self):
+        del self.lexer
+
+class LatexLexerTest(BaseLatexLexerTest):
+
+    def test_null(self):
+        self.lex_it(b'', [], final=True)
+
+    def test_hello(self):
+        self.lex_it(
+            b'hello!  [#1] This \\is\\   \\^ a \ntest.\n'
+            b'    \nHey.\n\n\# x \#x',
+            br'h|e|l|l|o|!| | |[|#1|]| |T|h|i|s| |\is|\ | | |\^| |a| '
+            b'|\n|t|e|s|t|.|\n| | | | |\n|H|e|y|.|\n|\n'
+            br'|\#| |x| |\#|x'.split(b'|'),
+            final=True
+            )
+
+    def test_comment(self):
+        self.lex_it(
+            b'test% some comment\ntest',
+            b't|e|s|t|% some comment\n|t|e|s|t'.split(b'|'),
+            final=True
+            )
+
+    def test_comment_newline(self):
+        self.lex_it(
+            b'test% some comment\n\ntest',
+            b't|e|s|t|% some comment\n|\n|t|e|s|t'.split(b'|'),
+            final=True
+            )
+
+    def test_control(self):
+        self.lex_it(
+            b'\\hello\\world',
+            b'\\hello|\\world'.split(b'|'),
+            final=True
+            )
+
+    def test_control_whitespace(self):
+        self.lex_it(
+            b'\\hello   \\world   ',
+            b'\\hello| | | |\\world| | | '.split(b'|'),
+            final=True
+            )
+
+    def test_controlx(self):
+        self.lex_it(
+            b'\\#\\&',
+            b'\\#|\\&'.split(b'|'),
+            final=True
+            )
+
+    def test_controlx_whitespace(self):
+        self.lex_it(
+            b'\\#    \\&   ',
+            b'\\#| | | | |\\&| | | '.split(b'|'),
+            final=True
+            )
+
+    def test_buffer(self):
+        self.lex_it(
+            b'hi\\t',
+            b'h|i'.split(b'|'),
+            )
+        self.lex_it(
+            b'here',
+            [b'\\there'],
+            final=True,
+            )
+
+    def test_state(self):
+        self.lex_it(
+            b'hi\\t',
+            b'h|i'.split(b'|'),
+            )
+        state = self.lexer.getstate()
+        self.lexer.reset()
+        self.lex_it(
+            b'here',
+            b'h|e|r|e'.split(b'|'),
+            final=True,
+            )
+        self.lexer.setstate(state)
+        self.lex_it(
+            b'here',
+            [b'\\there'],
+            final=True,
+            )
+
+    @nose.tools.raises(NotImplementedError)
+    def test_decode(self):
+            self.lexer.decode(b'')
+
+    def test_final_backslash(self):
+        self.lex_it(
+            b'notsogood\\',
+            b'n|o|t|s|o|g|o|o|d|\\'.split(b'|'),
+            final=True
+            )
+
+    def test_final_comment(self):
+        self.lex_it(
+            b'hello%',
+            b'h|e|l|l|o|%'.split(b'|'),
+            final=True
+            )
+
+    def test_hash(self):
+        self.lex_it(b'#', [b'#'], final=True)
 
 class BaseTexLexerTest(TestCase):
     """Tex lexer fixture."""
