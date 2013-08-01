55453dbefef31d9d9eed8f45640b932aa638e2cc
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-08-01 12:29:46 +0100

    Test the filters just added.

--- test/test_filter.py
+++ test/test_filter.py
@@ -25,3 +25,48 @@ def test_filter(app):
         assert re.search('Tralalala', output)
         assert not re.search('ideetje', output)
         assert not re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "or.html")) as stream:
+        output = stream.read()
+        assert not re.search('Tralalala', output)
+        assert re.search('ideetje', output)
+        assert re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "noteq.html")) as stream:
+        output = stream.read()
+        assert re.search('Tralalala', output)
+        assert re.search('ideetje', output)
+        assert not re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "lt.html")) as stream:
+        output = stream.read()
+        assert re.search('Tralalala', output)
+        assert not re.search('ideetje', output)
+        assert not re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "lte.html")) as stream:
+        output = stream.read()
+        assert re.search('Tralalala', output)
+        assert not re.search('ideetje', output)
+        assert re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "gt.html")) as stream:
+        output = stream.read()
+        assert not re.search('Tralalala', output)
+        assert re.search('ideetje', output)
+        assert not re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "gte.html")) as stream:
+        output = stream.read()
+        assert not re.search('Tralalala', output)
+        assert re.search('ideetje', output)
+        assert re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "key.html")) as stream:
+        output = stream.read()
+        assert not re.search('Tralalala', output)
+        assert re.search('ideetje', output)
+        assert not re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "false.html")) as stream:
+        output = stream.read()
+        assert not re.search('Tralalala', output)
+        assert not re.search('ideetje', output)
+        assert not re.search('Jakkamakka', output)
+    with open(os.path.join(app.outdir, "title.html")) as stream:
+        output = stream.read()
+        assert not re.search('Tralalala', output)
+        assert not re.search('ideetje', output)
+        assert re.search('Jakkamakka', output)
