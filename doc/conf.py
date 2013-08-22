121ad8c9cce989611facb44f80cb18bc9fa4a2a0
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-08-22 13:16:15 +0100

    Remove unused options in Sphinx docs. [ci skip]

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,7 @@ extensions = [
     'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -123,7 +123,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
