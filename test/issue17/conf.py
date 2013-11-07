605365d9067b893b9f4e7cf4b8ed04b5dd1d40b9
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-11-07 14:35:44 +0000

    Remove latex_elements.

--- test/issue17/conf.py
+++ test/issue17/conf.py
@@ -10,8 +10,6 @@ sys.path.insert(0, os.path.abspath('.'))
 
 source_suffix = '.rst'
 exclude_patterns = ['_build']
-latex_elements = {
-}
 texinfo_documents = [
     ('index', 'test', u'test Documentation',
      u'nobody', 'test', 'One line description of project.',
--- test/list_invalid/conf.py
+++ test/list_invalid/conf.py
@@ -1,8 +1,6 @@
 extensions = ['sphinxcontrib.bibtex']
 source_suffix = '.rst'
 exclude_patterns = ['_build']
-latex_elements = {
-}
 texinfo_documents = [
     ('index', 'test', u'test Documentation',
      u'nobody', 'test', 'One line description of project.',
