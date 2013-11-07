155e77a829eedadcbb0f94bd8194eeda51d19c1b
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-11-07 14:34:11 +0000

    Remove man_pages.

--- test/issue17/conf.py
+++ test/issue17/conf.py
@@ -12,10 +12,6 @@ source_suffix = '.rst'
 exclude_patterns = ['_build']
 latex_elements = {
 }
-man_pages = [
-    ('index', 'test', u'test Documentation',
-     [u'nobody'], 1)
-]
 texinfo_documents = [
     ('index', 'test', u'test Documentation',
      u'nobody', 'test', 'One line description of project.',
--- test/sphinx/conf.py
+++ test/sphinx/conf.py
@@ -1,8 +1,3 @@
 extensions = ['sphinxcontrib.bibtex']
 source_suffix = '.rst'
 exclude_patterns = ['_build']
-man_pages = [
-    ('index', 'sphinxbibtexextensiontest',
-     u'Sphinx bibtex extension test Documentation',
-     [u'Matthias C. M. Troffaes'], 1)
-]
