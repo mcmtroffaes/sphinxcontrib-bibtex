b849a7be8cf10fe03cacd7cd2bdf1a5223b19dcd
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-11-07 14:36:22 +0000

    Remove texinfo_documents.

--- test/custom_style/conf.py
+++ test/custom_style/conf.py
@@ -1,11 +1,6 @@
 extensions = ['sphinxcontrib.bibtex']
 source_suffix = '.rst'
 exclude_patterns = ['_build']
-texinfo_documents = [
-    ('index', 'test', u'test Documentation',
-     u'nobody', 'test', 'One line description of project.',
-     'Miscellaneous'),
-]
 
 # create and register pybtex plugins
 
--- test/invalid_cite_option/conf.py
+++ test/invalid_cite_option/conf.py
@@ -1,8 +1,3 @@
 extensions = ['sphinxcontrib.bibtex']
 source_suffix = '.rst'
 exclude_patterns = ['_build']
-texinfo_documents = [
-    ('index', 'test', u'test Documentation',
-     u'nobody', 'test', 'One line description of project.',
-     'Miscellaneous'),
-]
