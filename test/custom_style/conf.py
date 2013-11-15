dde9376f6fa2436b28ed210ae2cfd51c129b48f8
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2013-11-15 11:19:37 +0000

    Trying out an alternative plugin scheme for pybtex.

--- test/custom_style/conf.py
+++ test/custom_style/conf.py
@@ -3,6 +3,16 @@ exclude_patterns = ['_build']
 
 # create and register pybtex plugins
 
-import pkg_resources
-for dist in pkg_resources.find_distributions("plugins/"):
-    pkg_resources.working_set.add(dist)
+from pybtex.style.formatting.unsrt import Style as UnsrtStyle
+from pybtex.style.template import words
+from pybtex.plugin import plugin_registry_loader
+
+class NoWebRefStyle(UnsrtStyle):
+    name = 'nowebref'
+
+    def format_web_refs(self, e):
+        # the following is just one simple way to return an empty node
+        return words['']
+
+plugin_registry_loader.register_name(
+    'pybtex.style.formatting', 'nowebref', NoWebRefStyle)
