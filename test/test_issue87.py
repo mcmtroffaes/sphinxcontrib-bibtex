22615b834ce1f2dbd3093da64918fc46cc7eda50
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2015-10-20 13:25:24 +0100

    Do proper test of the output.

--- test/test_issue87.py
+++ test/test_issue87.py
@@ -20,6 +20,19 @@ def teardown_module():
 @with_app(srcdir=srcdir, warningiserror=True)
 def test_issue87(app, status, warning):
     app.builder.build_all()
-    with open(os.path.join(app.outdir, "contents.html")) as stream:
+    with open(os.path.join(app.outdir, "doc0.html")) as stream:
         output = stream.read()
-        # TODO test some things
+        assert re.search(
+            'class="reference internal" href="#tag0-2009-mandel"', output)
+        assert re.search(
+            'class="reference internal" href="#tag0-2003-evensen"', output)
+        assert re.search('AMan09', output)
+        assert re.search('AEve03', output)
+    with open(os.path.join(app.outdir, "doc1.html")) as stream:
+        output = stream.read()
+        assert re.search(
+            'class="reference internal" href="#tag1-2009-mandel"', output)
+        assert not re.search(
+            'class="reference internal" href="#tag1-2003-evensen"', output)
+        assert re.search('BMan09', output)
+        assert not re.search('BEve03', output)
