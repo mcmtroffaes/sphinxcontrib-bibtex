b23af347f95b35e6d14d193a9004a300a0c5999d
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-15 15:29:59 +0100

    Another test fixed.

--- test/test_issue14.py
+++ test/test_issue14.py
@@ -17,6 +17,12 @@ def teardown_module():
     (srcdir / '_build').rmtree(True)
 
 
+def htmlbiblabel(label):
+    return (
+        '<dt class="label".*><span class="brackets">{0}</span></dt>'
+        .format(label))
+
+
 @with_app(srcdir=srcdir)
 def test_duplicate_label(app, status, warning):
     app.builder.build_all()
@@ -24,6 +30,6 @@ def test_duplicate_label(app, status, warning):
         'duplicate label for keys (Test and Test2)|(Test2 and Test)',
         warning.getvalue())
     output = (path(app.outdir) / "doc1.html").read_text(encoding='utf-8')
-    assert re.search('<td class="label">\\[1\\]</td>', output)
+    assert re.search(htmlbiblabel("1"), output)
     output = (path(app.outdir) / "doc2.html").read_text(encoding='utf-8')
-    assert re.search('<td class="label">\\[1\\]</td>', output)
+    assert re.search(htmlbiblabel("1"), output)
