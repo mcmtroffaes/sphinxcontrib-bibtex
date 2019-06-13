62c8b4cc45ee8f9134a9bad4fd1657ecfb871b44
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-06-13 13:33:02 +0100

    Simplify sphinx test.

--- test/test_sphinx.py
+++ test/test_sphinx.py
@@ -6,7 +6,6 @@
     General Sphinx test and check output.
 """
 
-import re
 from sphinx_testing.util import path, with_app
 
 
@@ -21,15 +20,5 @@ def teardown_module():
 def test_sphinx(app, status, warning):
     app.builder.build_all()
     warnings = warning.getvalue()
-    assert not re.search(u'could not relabel citation \\[Test01\\]', warnings)
-    assert not re.search(u'could not relabel citation \\[Test02\\]', warnings)
-    assert not re.search(u'could not relabel citation \\[Wa04\\]', warnings)
-    assert not re.search(
-        u'could not relabel citation reference \\[Test01\\]',
-        warnings)
-    assert not re.search(
-        u'could not relabel citation reference \\[Test02\\]',
-        warnings)
-    assert not re.search(
-        u'could not relabel citation reference \\[Wa04\\]',
-        warnings)
+    assert u'could not relabel citation' not in warnings
+    assert u'is not referenced' in warnings
