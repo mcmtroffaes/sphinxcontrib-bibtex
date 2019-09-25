174777b9eaa9632fe6e3085cbd9cfab44d93214d
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-09-25 17:11:11 +0100

    Improve citation ordering code (see issue #15 and issue #182).

--- sphinxcontrib/bibtex/transforms.py
+++ sphinxcontrib/bibtex/transforms.py
@@ -21,7 +21,6 @@ from pybtex.plugin import find_plugin
 
 from sphinxcontrib.bibtex.nodes import bibliography
 
-
 logger = sphinx.util.logging.getLogger(__name__)
 
 
@@ -52,6 +51,15 @@ def transform_url_command(textnode):
         return textnode
 
 
+def get_docnames(env):
+    rel = env.collect_relations()
+    docname = env.config.master_doc
+    while docname is not None:
+        yield docname
+        parent, prevdoc, nextdoc = rel[docname]
+        docname = nextdoc
+
+
 class BibliographyTransform(docutils.transforms.Transform):
 
     """A docutils transform to generate citation entries for
@@ -71,12 +79,14 @@ class BibliographyTransform(docutils.transforms.Transform):
         """
         env = self.document.settings.env
         docname = env.docname
+        docnames = list(get_docnames(env))
         for bibnode in self.document.traverse(bibliography):
             id_ = bibnode['ids'][0]
             bibcache = env.bibtex_cache.get_bibliography_cache(
                 docname=docname, id_=id_)
             entries = env.bibtex_cache.get_bibliography_entries(
-                docname=docname, id_=id_, warn=logger.warning)
+                docname=docname, id_=id_, warn=logger.warning,
+                docnames=docnames)
             # locate and instantiate style and backend plugins
             style = find_plugin('pybtex.style.formatting', bibcache.style)()
             backend = find_plugin('pybtex.backends', 'docutils')()
