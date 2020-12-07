a8fb32b3520b02deb8a832fc7fb82df9b12cb068
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-07 17:21:55 +0000

    Another update.

--- test/test_custom_labels.py
+++ test/test_custom_labels.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-    test_custom_style
-    ~~~~~~~~~~~~~~~~~
+    test_custom_labels
+    ~~~~~~~~~~~~~~~~~~
 
     Test a custom label style.
 """
 
-from sphinx_testing.util import path, with_app
-
-srcdir = path(__file__).dirname().joinpath('custom_labels').abspath()
+import pytest
 
 
-def teardown_module():
-    (srcdir / '_build').rmtree(True)
-
-
-@with_app(srcdir=srcdir, warningiserror=True)
-def test_custom_labels(app, status, warning):
+@pytest.mark.sphinx('html', testroot='custom_labels')
+def test_custom_labels(app, warning):
     app.builder.build_all()
-    output = (path(app.outdir) / "index.html").read_text(encoding='utf-8')
+    assert not warning.getvalue()
+    output = (app.outdir / "index.html").read_text(encoding='utf-8')
     # the custom style uses keys as labels
-    print(output)
     assert "[myfancybibtexkey]" in output
     assert "[myotherfancybibtexkey]" in output
     assert ">myfancybibtexkey</a>" in output
