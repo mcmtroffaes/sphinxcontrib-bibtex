92a5cff47d27f3df77d4187b267a3456758b77d0
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-22 13:54:11 +0000

    Use self.target instead of  node['reftarget'].

--- sphinxcontrib/bibtex/roles.py
+++ sphinxcontrib/bibtex/roles.py
@@ -22,11 +22,8 @@ class CiteRole(XRefRole):
         """Transform reference node into a citation reference,
         and note that the reference was cited.
         """
-        keys = [key.strip() for key in node['reftarget'].split(',')]
-        # Note that at this point, usually, env.bibtex_cache.bibfiles
-        # is still empty because the bibliography directive may not
-        # have been processed yet, so we cannot get the actual entry.
-        # Instead, we simply fake an entry with the desired key, and
+        keys = [key.strip() for key in self.target.split(',')]
+        # We fake an entry with the desired key, and
         # fix the label at doctree-resolved time. This happens in
         # process_citation_references.
         refnodes = [
