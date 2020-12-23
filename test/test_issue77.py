ab973a90bb2c8494bb3427252c9c5ab4e584a65c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-23 19:47:12 +0000

    Update test_issue77.py

--- test/test_issue77.py
+++ test/test_issue77.py
@@ -14,5 +14,5 @@ def test_issue77(app, warning):
     app.builder.build_all()
     assert not warning.getvalue()
     output = (app.outdir / "index.html").read_text(encoding='utf-8')
-    assert len(re.findall('\\[APAa\\]', output)) == 1
-    assert len(re.findall('\\[APAb\\]', output)) == 1
+    assert len(re.findall('\\[APAa\\]', output)) == 2
+    assert len(re.findall('\\[APAb\\]', output)) == 2
