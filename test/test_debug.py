a22b4bf2a69fcf20f654abcc88429cad885bdcc0
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-26 08:46:03 +0000

    Add another debug test.

--- test/test_debug.py
+++ test/test_debug.py
@@ -5,8 +5,15 @@ to help understand what docutils/sphinx are doing.
 import pytest
 
 
-@pytest.mark.sphinx('html', testroot='debug_citation')
-def test_debug_citation(app, warning):
+@pytest.mark.sphinx('html', testroot='debug_docutils_citation')
+def test_debug_docutils_citation(app, warning):
+    """A simple test with a single standard docutils citation."""
+    app.build()
+    assert not warning.getvalue()
+
+
+@pytest.mark.sphinx('html', testroot='debug_bibtex_citation')
+def test_debug_bibtex_citation(app, warning):
     """A simple test with a single standard docutils citation."""
     app.build()
     assert not warning.getvalue()
