8d1cf3967a6f4bf4acbf8ffc150d044b23444f9b
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-29 10:51:40 +0000

    Remove re dependency from filter test.

--- test/test_filter.py
+++ test/test_filter.py
@@ -5,7 +5,6 @@
     Test filter option.
 """
 
-import re
 import pytest
 
 
@@ -14,46 +13,46 @@ def test_filter(app, warning):
     app.builder.build_all()
     assert not warning.getvalue()
     output = (app.outdir / "index.html").read_text()
-    assert re.search('Tralalala', output)
-    assert not re.search('ideetje', output)
-    assert not re.search('Jakkamakka', output)
+    assert 'Tralalala' in output
+    assert 'ideetje' not in output
+    assert 'Jakkamakka' not in output
     output = (app.outdir / "or.html").read_text()
-    assert not re.search('Tralalala', output)
-    assert re.search('ideetje', output)
-    assert re.search('Jakkamakka', output)
+    assert 'Tralalala' not in output
+    assert 'ideetje' in output
+    assert 'Jakkamakka' in output
     output = (app.outdir / "noteq.html").read_text()
-    assert re.search('Tralalala', output)
-    assert re.search('ideetje', output)
-    assert not re.search('Jakkamakka', output)
+    assert 'Tralalala' in output
+    assert 'ideetje' in output
+    assert 'Jakkamakka' not in output
     output = (app.outdir / "lt.html").read_text()
-    assert re.search('Tralalala', output)
-    assert not re.search('ideetje', output)
-    assert not re.search('Jakkamakka', output)
+    assert 'Tralalala' in output
+    assert 'ideetje' not in output
+    assert 'Jakkamakka' not in output
     output = (app.outdir / "lte.html").read_text()
-    assert re.search('Tralalala', output)
-    assert not re.search('ideetje', output)
-    assert re.search('Jakkamakka', output)
+    assert 'Tralalala' in output
+    assert 'ideetje' not in output
+    assert 'Jakkamakka' in output
     output = (app.outdir / "gt.html").read_text()
-    assert not re.search('Tralalala', output)
-    assert re.search('ideetje', output)
-    assert not re.search('Jakkamakka', output)
+    assert 'Tralalala' not in output
+    assert 'ideetje' in output
+    assert 'Jakkamakka' not in output
     output = (app.outdir / "gte.html").read_text()
-    assert not re.search('Tralalala', output)
-    assert re.search('ideetje', output)
-    assert re.search('Jakkamakka', output)
+    assert 'Tralalala' not in output
+    assert 'ideetje' in output
+    assert 'Jakkamakka' in output
     output = (app.outdir / "key.html").read_text()
-    assert not re.search('Tralalala', output)
-    assert re.search('ideetje', output)
-    assert not re.search('Jakkamakka', output)
+    assert 'Tralalala' not in output
+    assert 'ideetje' in output
+    assert 'Jakkamakka' not in output
     output = (app.outdir / "false.html").read_text()
-    assert not re.search('Tralalala', output)
-    assert not re.search('ideetje', output)
-    assert not re.search('Jakkamakka', output)
+    assert 'Tralalala' not in output
+    assert 'ideetje' not in output
+    assert 'Jakkamakka' not in output
     output = (app.outdir / "true.html").read_text()
-    assert re.search('Tralalala', output)
-    assert re.search('ideetje', output)
-    assert re.search('Jakkamakka', output)
+    assert 'Tralalala' in output
+    assert 'ideetje' in output
+    assert 'Jakkamakka' in output
     output = (app.outdir / "title.html").read_text()
-    assert not re.search('Tralalala', output)
-    assert not re.search('ideetje', output)
-    assert re.search('Jakkamakka', output)
+    assert 'Tralalala' not in output
+    assert 'ideetje' not in output
+    assert 'Jakkamakka' in output
