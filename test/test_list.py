e6873f792384a64c7885360c6a90b31f895adf5a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-29 13:20:46 +0000

    Merge list tests.

---
+++ test/test_list.py
@@ -0,0 +1,64 @@
+import common
+import pytest
+import re
+
+
+@pytest.mark.sphinx('html', testroot='list_citation')
+def test_list_citation(app, warning):
+    app.build()
+    assert not warning.getvalue()
+    output = (app.outdir / "index.html").read_text()
+    assert common.html_citations(label='1', text='.*Akkerdju.*').search(output)
+    assert common.html_citations(label='2', text='.*Bro.*').search(output)
+    assert common.html_citations(label='3', text='.*Chap.*').search(output)
+    assert common.html_citations(label='4', text='.*Dude.*').search(output)
+
+
+@pytest.mark.sphinx('html', testroot='list_bullet')
+def test_list_bullet(app, warning):
+    app.build()
+    assert not warning.getvalue()
+    output = (app.outdir / "index.html").read_text()
+    assert re.search(
+        '<ul'
+        '.*<li.*Akkerdju.*</li>'
+        '.*<li.*Bro.*</li>'
+        '.*<li.*Chap.*</li>'
+        '.*<li.*Dude.*</li>'
+        '.*</ul>',
+        output, re.DOTALL)
+
+
+@pytest.mark.sphinx('html', testroot='list_enumerated')
+def test_list_enumerated(app, warning):
+    app.build()
+    assert not warning.getvalue()
+    output = (app.outdir / "index.html").read_text()
+    assert re.search(
+        '<ol .*start="1".*>'
+        '.*<li.*Akkerdju.*</li>'
+        '.*<li.*Bro.*</li>'
+        '.*<li.*Chap.*</li>'
+        '.*<li.*Dude.*</li>'
+        '.*</ol>'
+        '.*<ol .*start="5".*>'
+        '.*<li.*Eminence.*</li>'
+        '.*<li.*Frater.*</li>'
+        '.*<li.*Giggles.*</li>'
+        '.*<li.*Handy.*</li>'
+        '.*</ol>'
+        '.*<ol .*start="23".*>'
+        '.*<li.*Iedereen.*</li>'
+        '.*<li.*Joke.*</li>'
+        '.*<li.*Klopgeest.*</li>'
+        '.*<li.*Laterfanter.*</li>'
+        '.*</ol>',
+        output, re.DOTALL)
+
+
+@pytest.mark.sphinx('html', testroot='list_invalid')
+def test_list_invalid(app, warning):
+    app.build()
+    assert re.search(
+        "unknown bibliography list type 'thisisintentionallyinvalid'",
+        warning.getvalue())
