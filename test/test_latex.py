d11dc58bc82ec560f577175dd82c2d1bbd97ec35
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-29 10:31:35 +0000

    Refactor and simplify test.

--- test/test_latex.py
+++ test/test_latex.py
@@ -23,9 +23,9 @@ def test_latex_multidoc(app, warning):
     app.build()
     assert not warning.getvalue()
     output = (app.outdir / "test.tex").read_text()
-    cits = common.latex_citations().finditer(output)
-    cit_refs = common.latex_citation_refs().finditer(output)
-    assert [cit.group('docname') for cit in cits] == ['sources']
-    assert [cit_ref.group('docname') for cit_ref in cit_refs] == ['sources']
-    assert ([cit.group('id_') for cit in cits] ==
-            [cit_ref.group('refid') for cit_ref in cit_refs])
+    assert len(common.latex_citations().findall(output)) == 1
+    assert len(common.latex_citation_refs().findall(output)) == 1
+    match = common.latex_citations().search(output)
+    match_ref = common.latex_citation_refs().search(output)
+    assert match.group('docname') == match_ref.group('docname') == 'sources'
+    assert match_ref.group('refid') == match.group('id_')
