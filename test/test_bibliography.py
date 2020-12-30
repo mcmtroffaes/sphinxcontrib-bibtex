cbd3e6afaa7e3f4b39f250ed2cc1127802886203
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-30 09:57:19 +0000

    Merge tests.

--- test/test_bibliography.py
+++ test/test_bibliography.py
@@ -22,6 +22,23 @@ def test_bibliography_empty_no_header(app, warning):
     assert output.count('<target') == 2
 
 
+@pytest.mark.sphinx('html', testroot='bibliography_style_label_1')
+def test_bibliography_style_label_1(app, warning):
+    app.build()
+    assert not warning.getvalue()
+    output = (app.outdir / "index.html").read_text()
+    # the custom style uses keys as labels
+    # citations
+    assert len(common.html_citations(
+        label='myfancybibtexkey').findall(output)) == 1
+    assert len(common.html_citations(
+        label='myotherfancybibtexkey').findall(output)) == 1
+    assert len(common.html_citation_refs(
+        label='myfancybibtexkey').findall(output)) == 1
+    assert len(common.html_citation_refs(
+        label='myotherfancybibtexkey').findall(output)) == 1
+
+
 @pytest.mark.sphinx('html', testroot='bibliography_style_label_2')
 def test_bibliography_style_label_2(app, warning):
     app.build()
