8dc669b500794d8e42f04836d1cc8dabd4d13797
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-30 09:16:24 +0000

    Simplify header test.

--- test/test_header.py
+++ test/test_header.py
@@ -1,12 +1,4 @@
-"""
-    test_header
-    ~~~~~~~~~~~
-
-    Test header config.
-"""
-
 import pytest
-import re
 
 
 @pytest.mark.sphinx('html', testroot='header')
@@ -14,11 +6,5 @@ def test_header(app, warning):
     app.build()
     assert not warning.getvalue()
     output = (app.outdir / "index.html").read_text(encoding='utf-8')
-    pattern1 = (
-        '<p class="rubric" '
-        'id="[^"]+">Regular Citations</p>')
-    pattern2 = (
-        '<p class="rubric" '
-        'id="[^"]+">Footnote Citations</p>')
-    assert re.search(pattern1, output) is not None
-    assert re.search(pattern2, output) is not None
+    assert 'Regular Citations' in output
+    assert 'Footnote Citations' in output
