dd847c4e92d853886e0963f40ad843067f7514a2
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-30 20:00:15 +0000

    Add test case for issue #226.

--- test/test_latex.py
+++ test/test_latex.py
@@ -30,3 +30,15 @@ def test_latex_multidoc(app, warning):
     assert match.group('docname') == match_ref.group('docname') == 'sources'
     assert match.group('id_') is not None
     assert match_ref.group('refid') == match.group('id_')
+
+
+@pytest.mark.skip('potential upstream sphinx problem, see issue #226')
+@pytest.mark.sphinx('html', testroot='latex_refs')
+def test_latex_after_html(make_app, app_params):
+    args, kwargs = app_params
+    app = make_app('html', freshenv=True, **kwargs)
+    app.build()
+    assert not app._warning.getvalue()
+    app = make_app('latex', freshenv=False, **kwargs)
+    app.build()
+    assert 'could not find bibtex key' not in app._warning.getvalue()
