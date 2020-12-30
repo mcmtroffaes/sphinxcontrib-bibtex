8c4b9aa16e9a0e8eb9e56d3b14ae92b5ce3e2ead
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-30 10:11:45 +0000

    Reuse functions.

--- test/test_bibliography.py
+++ test/test_bibliography.py
@@ -74,16 +74,12 @@ def test_bibliography_key_prefix(app, warning):
     app.build()
     assert not warning.getvalue()
     output = (app.outdir / "doc0.html").read_text()
-    cits = {match.group('label')
-            for match in common.html_citations().finditer(output)}
-    citrefs = {match.group('label')
-               for match in common.html_citation_refs().finditer(output)}
+    cits = get_citations(output)
+    citrefs = get_citation_refs(output)
     assert cits == citrefs == {'AMan09', 'AEve03'}
     output = (app.outdir / "doc1.html").read_text()
-    cits = {match.group('label')
-            for match in common.html_citations().finditer(output)}
-    citrefs = {match.group('label')
-               for match in common.html_citation_refs().finditer(output)}
+    cits = get_citations(output)
+    citrefs = get_citation_refs(output)
     assert cits == citrefs == {'BMan09'}
 
 
