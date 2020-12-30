445884906df22a11b26cde78bed0456b61e27778
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-30 10:00:41 +0000

    Merge tests.

--- test/test_bibliography.py
+++ test/test_bibliography.py
@@ -46,3 +46,13 @@ def test_bibliography_style_label_2(app, warning):
     output = (app.outdir / "index.html").read_text()
     assert len(common.html_citation_refs(label='APAa').findall(output)) == 1
     assert len(common.html_citation_refs(label='APAb').findall(output)) == 1
+
+
+@pytest.mark.sphinx('html', testroot='bibliography_style_nowebref')
+def test_bibliography_style_nowebref(app, warning):
+    app.build()
+    assert not warning.getvalue()
+    output = (app.outdir / "index.html").read_text(encoding='utf-8')
+    # the custom style suppresses web links
+    assert 'http://arxiv.org' not in output
+    assert 'http://dx.doi.org' not in output
