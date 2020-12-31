fd1622e6de8db8630eb8caea3cfa299a62b0842b
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-12-31 09:37:38 +0000

    Mark test as broken on older sphinx versions.

--- test/test_latex.py
+++ test/test_latex.py
@@ -1,5 +1,6 @@
 import common
 import pytest
+import sphinx
 
 
 @pytest.mark.sphinx('latex', testroot='latex_refs')
@@ -32,13 +33,15 @@ def test_latex_multidoc(app, warning):
     assert match_ref.group('refid') == match.group('id_')
 
 
-@pytest.mark.skip('potential upstream sphinx problem, see issue #226')
-@pytest.mark.sphinx('html', testroot='latex_refs')
+@pytest.mark.skipif(
+    sphinx.version_info < (3, 5, 0, 'beta'),
+    reason='broken on older sphinx versions')
+@pytest.mark.sphinx(testroot='latex_refs')
 def test_latex_after_html(make_app, app_params):
     args, kwargs = app_params
-    app = make_app('html', freshenv=True, **kwargs)
-    app.build()
-    assert not app._warning.getvalue()
-    app = make_app('latex', freshenv=False, **kwargs)
-    app.build()
-    assert 'could not find bibtex key' not in app._warning.getvalue()
+    app0 = make_app('html', freshenv=True, *args, **kwargs)
+    app0.build()
+    assert not app0._warning.getvalue()
+    app1 = make_app('latex', freshenv=False, *args, **kwargs)
+    app1.build()
+    assert 'could not find bibtex key' not in app1._warning.getvalue()
