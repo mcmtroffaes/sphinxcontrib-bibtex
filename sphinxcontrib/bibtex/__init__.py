dc5bb479f62166456d79e7a10c27e478346ff012
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-05-16 10:18:40 +0100

    Prepare release 2.2.1. (#247)

--- sphinxcontrib/bibtex/__init__.py
+++ sphinxcontrib/bibtex/__init__.py
@@ -42,7 +42,7 @@ def setup(app: Sphinx) -> Dict[str, Any]:
     app.add_role("footcite", FootCiteRole())
 
     return {
-        'version': '2.2.1a0',
+        'version': '2.2.1',
         'env_version': 7,
         'parallel_read_safe': True,
         'parallel_write_safe': True,
