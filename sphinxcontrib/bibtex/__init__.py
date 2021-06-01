61d77cac3cb636f01da2872cccb2c3993b80bb33
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2021-06-01 11:47:21 +0100

    Version bump. [skip ci]

--- sphinxcontrib/bibtex/__init__.py
+++ sphinxcontrib/bibtex/__init__.py
@@ -45,7 +45,7 @@ def setup(app: Sphinx) -> Dict[str, Any]:
     app.add_role("footcite", FootCiteRole())
 
     return {
-        'version': '2.3.0',
+        'version': '2.3.1a0',
         'env_version': 7,
         'parallel_read_safe': True,
         'parallel_write_safe': True,
