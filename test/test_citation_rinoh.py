11b0c326be090bfdd9c2a2660cc9c96b594402b7
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2022-03-30 10:12:41 +0100

    Inspect rinohtype's stylelog files to check citations are present as expected.

---
+++ test/test_citation_rinoh.py
@@ -0,0 +1,26 @@
+import re
+import pytest
+
+
+@pytest.mark.rinohtype
+@pytest.mark.sphinx('rinoh', testroot='citation_rinoh')
+def test_citation_rinoh(app, warning) -> None:
+    app.build()
+    assert not warning.getvalue()
+    output = (app.outdir / "book.stylelog").read_text()
+    assert len(re.findall(
+        r"NoteMarkerByID\('tes', style='citation'\)", output)) == 1
+    assert len(re.findall(
+        r"DirectReference\('tes'\)", output)) == 1
+
+
+@pytest.mark.rinohtype
+@pytest.mark.sphinx('rinoh', testroot='citation_rinoh_multidoc')
+def test_citation_rinoh_multidoc(app, warning) -> None:
+    app.build()
+    assert not warning.getvalue()
+    output = (app.outdir / "book.stylelog").read_text()
+    assert len(re.findall(
+        r"NoteMarkerByID\('tes', style='citation'\)", output)) == 2
+    assert len(re.findall(
+        r"DirectReference\('tes'\)", output)) == 1
