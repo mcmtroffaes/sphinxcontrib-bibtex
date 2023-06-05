3b88bacbe6a6e82480c6c134cb3606905e805f67
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2023-06-05 18:58:47 +0100

    Remove old monkey patch.

--- test/conftest.py
+++ test/conftest.py
@@ -9,8 +9,3 @@ collect_ignore = ['roots']
 @pytest.fixture(scope='session')
 def rootdir() -> path:
     return path(__file__).parent.abspath() / 'roots'
-
-
-# monkey patch for path class on old sphinx versions
-if not hasattr(path, "read_text"):
-    path.read_text = path.text  # type: ignore
