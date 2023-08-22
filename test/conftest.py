3f101b3f4d285dda8225b37a440332ff268c6a5f
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2023-08-22 09:12:28 +0200

    Make mypy happy.

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 import sphinx
 
-if sphinx.version_info < (7, 2):
-    from sphinx.testing.path import path as Path
-    _rootdir = Path(__file__).parent.abspath() / 'roots'
-else:
+if sphinx.version_info >= (7, 2):
     from pathlib import Path
     _rootdir = Path(__file__).parent.resolve() / 'roots'
+else:
+    from sphinx.testing.path import path as Path  # type: ignore
+    _rootdir = Path(__file__).parent.abspath() / 'roots'  # type: ignore
 
 pytest_plugins = 'sphinx.testing.fixtures'
 collect_ignore = ['roots']
