a76bc6187f7e5bb55a10c0fb21abd68fc7c98ba4
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-11 17:07:38 +0100

    Fix version in sphinx conf.

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,9 @@
-import re
+import sys
+
+if sys.version_info >= (3, 10):
+    from importlib.metadata import version
+else:
+    from importlib_metadata import version
 
 needs_sphinx = "2.1"
 extensions = [
@@ -10,8 +15,7 @@ extensions = [
 master_doc = "index"
 project = "sphinxcontrib-bibtex"
 copyright = "2011-2023, Matthias C. M. Troffaes"
-with open("../src/sphinxcontrib/bibtex/__init__.py", "rt") as version_file:
-    release = re.search('"version": "(.+)"', version_file.read()).group(1)
+release = version("sphinxcontrib-bibtex")
 version = ".".join(release.split(".")[:2])
 exclude_patterns = ["_build"]
 pygments_style = "sphinx"
