52a06a31888ed0847c15e12f9697b9800118764c
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2026-04-21 16:26:26 +0100

    Fix docutils warning.

--- src/sphinxcontrib/bibtex/domain.py
+++ src/sphinxcontrib/bibtex/domain.py
@@ -226,9 +226,7 @@ class Citation(NamedTuple):
 def parse_header(header: str, source_path: str):
     parser = docutils.parsers.rst.Parser()
     # note: types stub for docutils doesn't know about components argument
-    settings = docutils.frontend.OptionParser(
-        components=(docutils.parsers.rst.Parser,)  # type: ignore
-    ).get_default_values()
+    settings = docutils.frontend.get_default_settings(docutils.parsers.rst.Parser)  # type: ignore[arg-type]
     document = docutils.utils.new_document(source_path, settings)
     parser.parse(header, document)
     return document[0]
